ce97cfe3fd1eea91660f90f61c17aa35083c2a86
Pratyush Sahay <pratyush97@gmail.com>
2022-12-21 00:50:40 -0500

    added firebase token as repo secret and reference github secret through package.json during build

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to psahay.net
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/pratyushsahay"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github
         </a>
       </header>
     </div>

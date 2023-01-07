a79d360adf9c82090f50d5c4cd7e9e6fbc63b5cb
Pratyush Sahay <pratyush97@gmail.com>
2023-01-07 02:08:44 -0500

    layout foundation for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to psahay.net
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/pratyushsahay"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Github
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+interface ButtonProps {
+    readonly children?: any,
+    readonly type?: any,
+    readonly onClick?: any,
+    readonly buttonStyle?: any,
+    readonly buttonSize?: any
+}
+
+const STYLES = ['btn--primary', 'btn--outline'];
+const SIZES = ['btn--medium', 'btn--large'];
+
+export function Button(props: ButtonProps) {
+    const checkButtonStyle = STYLES.includes(props.buttonStyle) ? props.buttonStyle : STYLES[0];
+    const checkButtonSize = SIZES.includes(props.buttonSize) ? props.buttonSize : SIZES[0];
+    
+    return (
+        <Link to='/sign-up' className="btn-mobile">
+            <button 
+                className={`btn ${checkButtonStyle} ${checkButtonSize}`} 
+                onClick={props.onClick} 
+                type={props.type}
+            >
+                {props.children}
+            </button>
+        </Link>
+    )
+}
